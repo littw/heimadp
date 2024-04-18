a53e3c58ee701946d6729bc0163b997f57a1e797
yty <tongyan812@qq.com>
2024-04-18 15:29:54 +0800

    利用Redis的自增机制生成多线程并发下的全局唯一id

---
+++ hm-dianping/src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -0,0 +1,39 @@
+package com.hmdp.utils;
+
+import org.apache.catalina.util.Introspection;
+import org.springframework.context.event.ApplicationEventMulticaster;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.Stack;
+
+@Component
+public class RedisIdWorker {
+    @Resource
+    private StringRedisTemplate stringRedisTemplate ;
+    private static final long BEGIN_TIME_STAMP=1704067200L;  //开始的时间戳
+    private static final int COUNT_BITS=32;
+    public Long nextId(String keyPrefix){
+        //生成时间戳
+        LocalDateTime now = LocalDateTime.now();
+        long second = now.toEpochSecond(ZoneOffset.UTC);
+        long timeStamp = second - BEGIN_TIME_STAMP;
+
+        //生成序列号
+        String date = now.format(DateTimeFormatter.ofPattern("yyyy:MM:dd")); //2024:01:01
+        Long count = stringRedisTemplate.opsForValue().increment("icr:" + keyPrefix + ":" + date);
+
+        //拼接返回
+        return timeStamp<<COUNT_BITS|count;
+    }
+
+    public static void main(String[] args) {
+        LocalDateTime time=LocalDateTime.of(2024,1,1,0,0,0);
+        long second = time.toEpochSecond(ZoneOffset.UTC);
+        System.out.println(second);
+    }
+}
--- hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -2,20 +2,48 @@ package com.hmdp;
 
 import com.hmdp.entity.Shop;
 import com.hmdp.service.impl.ShopServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
     @Resource
     private ShopServiceImpl shopService;
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+    //500个线程的线程池
+    private ExecutorService es=Executors.newFixedThreadPool(500);
 
     @Test
     public void testSaveShop() throws InterruptedException {
         shopService.saveShop2Redis(1L,10L);
     }
 
+    @Test
+    public void testIdWorker() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(300);
+        //创建订单号
+        Runnable task=()->{
+            for (int i = 0; i < 100; i++) {
+                Long id = redisIdWorker.nextId("order");
+                System.out.println("id="+id);
+            }
+            latch.countDown();
+        };
+        long begin = System.currentTimeMillis();
+        for (int i = 0; i < 300; i++) {
+            es.submit(task);
+        }
+        latch.await();
+        long end=System.currentTimeMillis();
+        System.out.println("time="+(end-begin));
+    }
 }
